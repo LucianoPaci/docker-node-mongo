69b98a7d47b08697decfefee2a3f8c7cb8c052bd
Luciano Paci <lucianopaci93@gmail.com>
2020-04-25 12:57:49 -0300

    first commit

--- models/Item.js
+++ models/Item.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const ItemSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
-});
+    default: Date.now,
+  },
+})
 
-module.exports = Item = mongoose.model('item', ItemSchema);
+module.exports = Item = mongoose.model('item', ItemSchema)
